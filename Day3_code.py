c91fde9c80ff69a10d1bcbf4a4a12d88230c195a
Soyoon Moon <soyoonmoon23@gmail.com>
2023-01-03 23:09:43 -0600

    Day3 done!

--- Day3_code.py
+++ Day3_code.py
@@ -17,13 +17,27 @@ with open('/Users/student/Desktop/Advent of Code/Day3.txt') as f:
 # part2
 with open('/Users/student/Desktop/Advent of Code/Day3.txt') as f:
     total = 0
+    final_list = []
     lines = f.readlines()
-    first_dict = {}
-    second_dict = {}
+    print(len(lines))
+
     for i in range(0, len(lines), 3):
-        first = set(lines[i])
-        second = set(lines[i+1])
-        third = dict(set(lines[i+2]))
-        print(first)
-        print(second)
-        print(third)
+        first_dict = {}
+        second_dict = {}
+        third_dict = {}
+        for key in lines[i]:
+            first_dict[key] = 0
+        for value in lines[i+1]:
+            if value in first_dict:
+                second_dict[value] = 0
+        for value in lines[i+2]:
+            if value in second_dict:
+                third_dict[value] = 0
+        for value in third_dict:
+            final_list.append(value)
+    for value in final_list:
+        if ord(value) >= 97 and ord(value) <= 122:
+            total += ord(value)-96
+        elif ord(value) >= 65 and ord(value) <= 90:
+            total += ord(value)-38
+    print(total)
